d93429bca153ba40799e5aabcf1df179c260383e
Joseph Avila Alvarez <josdavil@espol.edu.ec>
2020-12-17 18:49:12 -0500

    update connect db

--- templates/src/connection_db.py
+++ templates/src/connection_db.py
@@ -8,8 +8,7 @@ from dns import resolver
 from datetime import date
 
 
-client = MongoClient(
-    "mongodb://admin:hgr4BvOeOME9R852@cluster0-shard-00-00.qmxmg.mongodb.net:27017,cluster0-shard-00-01.qmxmg.mongodb.net:27017,cluster0-shard-00-02.qmxmg.mongodb.net:27017/sophia?ssl=true&replicaSet=atlas-vb7337-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = MongoClient("##")
 db = client.get_database("sophia")
 collection = db.queries
 """
